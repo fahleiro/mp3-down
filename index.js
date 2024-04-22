111b8f46f4d7040b4f367e6ca9a1fd90bc70e7ed
gabriel.faleiro <gabriel.faleiro@dbserver.com.br>
2024-04-22 12:10:09 -0300

    upload project

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const ytdl = require('ytdl-core');
+const removeAccents = require('remove-accents');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>YouTube Audio Downloader</h1>
+    <form action="/download" method="post">
+      <label for="url">URL do vídeo do YouTube:</label><br>
+      <input type="text" id="url" name="url" required><br>
+      <button type="submit">Baixar Áudio</button>
+    </form>
+  `);
+});
+
+app.post('/download', async (req, res) => {
+  const url = req.body.url;
+  try {
+    const info = await ytdl.getInfo(url);
+    let title = info.videoDetails.title;
+    title = removeAccents(title); // Remove acentos gráficos
+    title = title.replace(/[^\w\s]/gi, ''); // Remove caracteres especiais do título
+    res.set('Content-Disposition', `attachment; filename="${title}.mp3"`);
+    ytdl(url, { filter: 'audioonly' }).pipe(res);
+  } catch (error) {
+    console.error('Erro ao baixar áudio:', error);
+    res.status(500).send('Erro ao baixar áudio');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor iniciado em http://localhost:${port}`);
+});
